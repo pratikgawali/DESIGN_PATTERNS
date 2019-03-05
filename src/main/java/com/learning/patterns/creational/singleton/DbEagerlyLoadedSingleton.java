1124dd2b3174d424ca1292780aef6587e5433508
Pratik Gawali <pratik.gawali@sap.com>
2019-03-05 20:08:05 +0530

    Added pros cons comments to EagerlyLoadedSingleton class

--- src/main/java/com/learning/patterns/creational/singleton/DbEagerlyLoadedSingleton.java
+++ src/main/java/com/learning/patterns/creational/singleton/DbEagerlyLoadedSingleton.java
@@ -3,9 +3,7 @@ package com.learning.patterns.creational.singleton;
 /**
  * Demonstrates eagerly loaded implementation of singleton design pattern.
  * 
- * 
  * @author I341876
- *
  */
 public class DbEagerlyLoadedSingleton {
 
@@ -27,3 +25,16 @@ public class DbEagerlyLoadedSingleton {
 	}
 
 }
+
+/*
+ *----------------------------------------------------------------------------------------------------------------
+ * PROs: 
+ * ------
+ * 		1. Thread Safe (Class loading by JVM is thread safe)
+ * ----------------------------------------------------------------------------------------------------------------
+ * CONs: 
+ * ------
+ * 		1. Unnecessary instantiation of singleton when instance creation is very expensive
+ * 		   operation and it is uncertain whether the instance would be used at all.
+ * ----------------------------------------------------------------------------------------------------------------
+ */
